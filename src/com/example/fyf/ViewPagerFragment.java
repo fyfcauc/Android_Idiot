c28a69b40bbec9c11032ca1e2042223892981d79
yfeng <yfeng@opera.com>
2014-12-11 13:45:57 +0800

    [make viewPager work in Fragment]

--- src/com/example/fyf/ViewPagerFragment.java
+++ src/com/example/fyf/ViewPagerFragment.java
@@ -2,7 +2,6 @@ package com.example.fyf;
 
 import java.util.ArrayList;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.PagerAdapter;
@@ -11,31 +10,32 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class ViewPagerFragment extends Fragment {
 
 	ViewPager mViewPager;
+	ArrayList<View> mImageArrayList = new ArrayList<View>();
+
+	private void generateData() {
+		Log.e("TEST", "generateData()");
+		ImageView image1 = new ImageView(getActivity().getApplicationContext());
+		image1.setImageResource(R.drawable.abc_ic_voice_search);
+		ImageView image2 = new ImageView(getActivity().getApplicationContext());
+		image2.setImageResource(R.drawable.abc_ic_cab_done_holo_dark);
+		ImageView image3 = new ImageView(getActivity().getApplicationContext());
+		image3.setImageResource(R.drawable.ic_launcher);
+//		LayoutInflater inflater = (LayoutInflater) getActivity().getApplicationContext().
+//                getSystemService(
+//                Context.LAYOUT_INFLATER_SERVICE);
+//		mImageArrayList.add(inflater.inflate(R.layout.fragment2, null));
+//		mImageArrayList.add(inflater.inflate(R.layout.fragment1, null));
+		mImageArrayList.add(image1);
+		mImageArrayList.add(image2);
+		mImageArrayList.add(image3);
+	}
+
 	PagerAdapter mPagerAdapter = new PagerAdapter() {
-		
-		ArrayList<View> mImageArrayList = new ArrayList<View>();
-		private boolean inited = false;
-		
-		private void generateData() {
-			Log.e("TEST", "generateData()");
-//			ImageView image1 = new ImageView(getActivity().getApplicationContext());
-//			image1.setImageResource(R.drawable.abc_ic_voice_search);
-//			ImageView image2 = new ImageView(getActivity().getApplicationContext());
-//			image1.setImageResource(R.drawable.abc_ic_cab_done_holo_dark);
-//			ImageView image3 = new ImageView(getActivity().getApplicationContext());
-//			image1.setImageResource(R.drawable.ic_launcher);
-			LayoutInflater inflater = (LayoutInflater) getActivity().getApplicationContext().
-	                getSystemService(
-	                Context.LAYOUT_INFLATER_SERVICE);
-			mImageArrayList.add(inflater.inflate(R.layout.fragment2, null));
-			mImageArrayList.add(inflater.inflate(R.layout.fragment1, null));
-//			mImageArrayList.add(image3);
-			inited = true;
-		}
 		
 		@Override
 		public boolean isViewFromObject(View arg0, Object arg1) {
@@ -51,9 +51,6 @@ public class ViewPagerFragment extends Fragment {
 		
 		@Override
 		public Object instantiateItem(ViewGroup container, int position) {
-			if (!inited) {
-				generateData();
-			}
 			Log.e("TEST", "instantiateItem() " + position);
 	        if (position >= 0 && position < mImageArrayList.size()) {
 	        		container.addView(mImageArrayList.get(position));
@@ -79,14 +76,15 @@ public class ViewPagerFragment extends Fragment {
 		super.onCreateView(inflater, container, savedInstanceState);
 		View rootView = inflater.inflate(R.layout.fragment3, container, false);
 //		mViewPager = (ViewPager)rootView.findViewWithTag(R.tag.viewPager1Tag);
+		generateData();
 		mViewPager = (ViewPager)rootView.findViewById(R.id.viewPager1);
+		mViewPager.setAdapter(mPagerAdapter);
+		mViewPager.setCurrentItem(0);
 		return rootView;
 	}
 	
 	@Override
 	public void onResume() {
 		super.onResume();
-		mViewPager.setAdapter(mPagerAdapter);
-		mViewPager.setCurrentItem(0);
 	}
 }

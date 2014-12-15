145d63eae9e01bd2e6678e6755ef90a5582faeb6
yfeng <yfeng@opera.com>
2014-12-15 17:43:16 +0800

    [try draw drawable on View]

--- src/com/example/fyf/DrawableTestView.java
+++ src/com/example/fyf/DrawableTestView.java
@@ -2,13 +2,15 @@ package com.example.fyf;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class DrawableTestView extends View {
 
+	private ShapeDrawable mDrawable;
 	public DrawableTestView(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
@@ -29,11 +31,16 @@ public class DrawableTestView extends View {
 	protected void onDraw(Canvas canvas) {
 		// TODO Auto-generated method stub
 //		super.onDraw(canvas);
-		Paint p = new Paint();
-		p.setColor(Color.rgb(255, 0, 0));
-		float cx, cy, radius;
-        cx = cy = getWidth() / 2.0f;
-        radius = getWidth() / 2.8f;
-        canvas.drawCircle(cx, cy, radius, p);
+		mDrawable = new ShapeDrawable(new OvalShape());
+	    mDrawable.getPaint().setColor(0x33ff0000);
+//	    mDrawable.setBounds(0, 0, 300, 300);
+	    mDrawable.setBounds(new Rect(100, 100, 200, 400));
+	    mDrawable.draw(canvas);
+//	    Paint p = new Paint();
+//		p.setColor(Color.rgb(255, 0, 0));
+//		float cx, cy, radius;
+//        cx = cy = getWidth() / 2.0f;
+//        radius = getWidth() / 2.8f;
+//        canvas.drawCircle(cx, cy, radius, p);
 	}
 }

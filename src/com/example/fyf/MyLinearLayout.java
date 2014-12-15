17f98989c8d4851ccec51c52480da75ad86fcfb0
yfeng <yfeng@opera.com>
2014-12-15 14:38:11 +0800

    [change to false can not work for custom view]

--- src/com/example/fyf/MyLinearLayout.java
+++ src/com/example/fyf/MyLinearLayout.java
@@ -32,7 +32,7 @@ public class MyLinearLayout extends LinearLayout {
 	private ImageView mImageView;
 	
 	private void init(Context context, AttributeSet attrs) {
-		LayoutInflater.from(context).inflate(R.layout.my_linear_layout, this, true);
+		LayoutInflater.from(context).inflate(R.layout.my_linear_layout, this, false);
 		mTextView = (TextView)this.findViewById(R.id.myTextView);
 		mImageView = (ImageView)this.findViewById(R.id.myImageView);
 		

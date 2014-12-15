108b51b79ced430bafb38b2e2964ecbfc4d5d1a0
yfeng <yfeng@opera.com>
2014-12-15 10:59:03 +0800

    [Add print stack function]

--- src/com/example/fyf/SystemUtils.java
+++ src/com/example/fyf/SystemUtils.java
@@ -1,6 +1,7 @@
 package com.example.fyf;
 
 import android.content.Context;
+import android.util.Log;
 
 public class SystemUtils {
 	private static Context sAppContext;
@@ -12,4 +13,11 @@ public class SystemUtils {
 	public Context getAppContext() {
 		return sAppContext;
 	}
+	
+	public static void printStack(final String tag) {
+        StackTraceElement elements[] = Thread.currentThread().getStackTrace();
+        for (StackTraceElement element : elements) {
+            Log.e(tag, element.toString());
+        }
+    }
 }

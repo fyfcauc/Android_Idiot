9a9f27679b3acd8e92ebea24aa28be16f90201ef
yfeng <yfeng@opera.com>
2014-12-17 10:02:20 +0800

    Try scroller and setContentView with view directly

---
+++ src/com/example/fyf/ScrollerActivity.java
@@ -0,0 +1,59 @@
+package com.example.fyf;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Scroller;
+
+public class ScrollerActivity extends Activity {
+
+	private Scroller mScroller; 
+	private static final String TAG = "ScrollerActivity";
+	public ScrollerActivity() {
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void onCreate(android.os.Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		LinearLayout l1 = new LinearLayout(this);
+//		LinearLayout l2 = new LinearLayout(this);
+		LinearLayout.LayoutParams p0 = new LinearLayout.LayoutParams    
+		        (LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+		l1.setOrientation(LinearLayout.VERTICAL);
+		l1.setLayoutParams(p0);
+		mScroller = new Scroller(this);
+		setContentView(l1);
+		mScroller.startScroll(-500, -500, 500, 500, 10000);
+		MyButton button = new MyButton(this);
+		button.setText("I AM MOVING, HAAAAAAAAAAAAAAAA");
+		button.setTextColor(Color.rgb(255, 0, 0));
+		button.setTextSize(25);
+		//		button.setBackgroundResource(R.drawable.home);
+		button.setLayoutParams(p0);
+		l1.addView(button);
+		
+	};
+	
+	private class MyButton extends Button {
+		public MyButton(Context ctx) {
+			// TODO Auto-generated constructor stub
+			super(ctx);
+		}
+		
+		@Override
+		public void computeScroll() {
+			// TODO Auto-generated method stub
+			super.computeScroll();
+			if (mScroller.computeScrollOffset()) {
+				int curx = mScroller.getCurrX();
+				int cury = mScroller.getCurrY();
+				Log.e(TAG, "computeScroll " + curx + " " + cury);
+				scrollTo(curx, cury);
+				postInvalidate();
+			}
+		}
+	}
+}

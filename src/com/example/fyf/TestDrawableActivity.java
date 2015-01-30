a88151a95ba378a6ac256a35bf95080295218154
yfeng <yfeng@opera.com>
2015-01-30 14:01:35 +0800

    test requestDisallowInterceptTouch

--- src/com/example/fyf/TestDrawableActivity.java
+++ src/com/example/fyf/TestDrawableActivity.java
@@ -5,15 +5,70 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.view.MotionEventCompat;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class TestDrawableActivity extends Activity {
 
+	public static class myLinearLayout extends LinearLayout {
+
+		public myLinearLayout(Context context) {
+			super(context);
+			// TODO Auto-generated constructor stub
+		}
+		
+		public myLinearLayout(Context context, AttributeSet attrs) {
+			super(context, attrs);
+			// TODO Auto-generated constructor stub
+		}
+		
+		public myLinearLayout(Context context, AttributeSet attrs, int defStyle) {
+			super(context, attrs, defStyle);
+			// TODO Auto-generated constructor stub
+		}
+		
+		@Override
+		public boolean onInterceptTouchEvent(MotionEvent ev) {
+			// TODO Auto-generated method stub
+//			return super.onInterceptTouchEvent(ev);
+			int action = MotionEventCompat.ACTION_MASK & ev.getAction();
+			
+			if (action == MotionEvent.ACTION_DOWN) {
+				return false;
+			} else {
+				return true;
+			}
+		}
+		
+		@Override
+		public boolean onTouchEvent(MotionEvent event) {
+			// TODO Auto-generated method stub
+			Log.e("FYF", "myLinearLayout " + SystemUtils.motionEventToString(event));
+			return true;
+		}
+		
+	}
+	
+	
 	public static class myView extends View {
 
+		@Override
+		public boolean onTouchEvent(MotionEvent event) {
+			// TODO Auto-generated method stub
+			int action = MotionEventCompat.ACTION_MASK & event.getAction();
+			Log.e("FYF", "myView " + SystemUtils.motionEventToString(event));
+			if (action == MotionEvent.ACTION_DOWN) {
+				getParent().requestDisallowInterceptTouchEvent(true);
+			}
+			
+			return true;
+		}
+		
 		public myView(Context context) {
 			super(context);
 			// TODO Auto-generated constructor stub
